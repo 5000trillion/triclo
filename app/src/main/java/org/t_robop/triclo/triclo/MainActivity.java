964dbe231c83d8dcc47236b135b38250dca44611
multiverse2011 <multiverse2011@gmail.com>
2017-06-18 23:10:01 +0900

    themeの修正

--- app/src/main/java/org/t_robop/triclo/triclo/MainActivity.java
+++ app/src/main/java/org/t_robop/triclo/triclo/MainActivity.java
@@ -10,7 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

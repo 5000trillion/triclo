c5908d86d56f7f0ca6093ec7a19d9c70fab2fad4
multiverse2011 <multiverse2011@gmail.com>
2017-06-20 18:07:44 +0900

    各画面の修正

--- app/src/main/java/org/t_robop/triclo/triclo/CameraActivity.java
+++ app/src/main/java/org/t_robop/triclo/triclo/CameraActivity.java
@@ -1,8 +1,28 @@
 package org.t_robop.triclo.triclo;
 
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+
 /**
  * Created by siilo on 2017/06/20.
  */
 
-public class CameraActivity {
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        //Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
 }
--- app/src/main/java/org/t_robop/triclo/triclo/ClRegistActivity.java
+++ app/src/main/java/org/t_robop/triclo/triclo/ClRegistActivity.java
@@ -1,8 +1,27 @@
 package org.t_robop.triclo.triclo;
 
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+
 /**
  * Created by tomi on 2017/06/20.
  */
+public class ClRegistActivity extends AppCompatActivity {
 
-public class ClRegistActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clregist);
+
+        //Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
 }

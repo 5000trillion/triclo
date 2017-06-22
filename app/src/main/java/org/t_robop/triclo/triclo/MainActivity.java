e2d3eccc9e641a4a795b52cdeb891c18c4dd86b0
Ishihara1222 <7bjt1222@mail.u-tokai.ac.jp>
2017-06-22 19:39:09 +0900

    gridview動きました

--- app/src/main/java/org/t_robop/triclo/triclo/MainActivity.java
+++ app/src/main/java/org/t_robop/triclo/triclo/MainActivity.java
@@ -1,5 +1,6 @@
 package org.t_robop.triclo.triclo;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -9,6 +10,15 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,6 +54,34 @@ public class MainActivity extends AppCompatActivity {
                 DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawerLayout);
                 drawer.closeDrawer(GravityCompat.START);
                 return true;
+            }
+        });
+
+        ArrayList<String> list = new ArrayList<String>();
+
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.add("D");
+        list.add("E");
+        list.add("F");
+        list.add("G");
+        list.add("H");
+        list.add("I");
+        list.add("J");
+        list.add("K");
+        list.add("L");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getApplicationContext(), android.R.layout.simple_list_item_1, list);
+
+        GridView gridView = (GridView) findViewById(R.id.gridView1);
+        gridView.setAdapter(adapter);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(),"FABが押されました",Toast.LENGTH_SHORT).show();
             }
         });
     }

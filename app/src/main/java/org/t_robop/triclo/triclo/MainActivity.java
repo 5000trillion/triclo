1641cdf8a436117b22543d5094ea93246947b1d5
Ishihara1222 <7bjt1222@mail.u-tokai.ac.jp>
2017-06-27 20:50:08 +0900

    fab動きました

--- app/src/main/java/org/t_robop/triclo/triclo/MainActivity.java
+++ app/src/main/java/org/t_robop/triclo/triclo/MainActivity.java
@@ -16,11 +16,19 @@ import java.util.ArrayList;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+
+    private Boolean isFabOpen = false;
+    private FloatingActionButton fab,fab1,fab2;
+    private Animation fab_open,fab_close,rotate_forward,rotate_backward;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Toolbar
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar1);
         setSupportActionBar(toolbar);
 
         //Drawer
@@ -78,11 +86,72 @@ public class MainActivity extends AppCompatActivity {
         GridView gridView = (GridView) findViewById(R.id.gridView1);
         gridView.setAdapter(adapter);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Toast.makeText(v.getContext(),"FABが押されました",Toast.LENGTH_SHORT).show();
-            }
-        });
+
+
+
+            Toolbar toolBar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolBar);
+        fab = (FloatingActionButton)findViewById(R.id.fab);
+        fab1 = (FloatingActionButton)findViewById(R.id.fab1);
+        fab2 = (FloatingActionButton)findViewById(R.id.fab2);
+        fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_close);
+        rotate_forward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_forward);
+        rotate_backward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_backward);
+        fab.setOnClickListener(this);
+        fab1.setOnClickListener(this);
+        fab2.setOnClickListener(this);
+
+
     }
+
+        public void onClick(View v) {
+            int id = v.getId();
+            switch (id){
+                case R.id.fab:
+                    animateFAB();
+                    break;
+                case R.id.fab1:
+                    Log.d("Raj", "Fab 1");
+                    break;
+                case R.id.fab2:
+                    Log.d("Raj", "Fab 2");
+                    break;
+            }
+        }
+
+        public void animateFAB(){
+
+            if(isFabOpen){
+
+                fab.startAnimation(rotate_backward);
+                fab1.startAnimation(fab_close);
+                fab2.startAnimation(fab_close);
+                fab1.setClickable(false);
+                fab2.setClickable(false);
+                isFabOpen = false;
+                Log.d("Raj", "close");
+
+            } else {
+
+                fab.startAnimation(rotate_forward);
+                fab1.startAnimation(fab_open);
+                fab2.startAnimation(fab_open);
+                fab1.setClickable(true);
+                fab2.setClickable(true);
+                isFabOpen = true;
+                Log.d("Raj","open");
+
+            }
+
+
+
+
+        }
 }
+
+
+
+
+
+

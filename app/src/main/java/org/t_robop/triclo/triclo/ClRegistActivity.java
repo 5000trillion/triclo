015f2cf1003d7478a49e3c4a778d95e9c4df094c
Thiroaki <zeus1447@gmail.com>
2017-07-06 19:49:45 +0900

    DatepickerDialogの修正と色選択ダイアログ作った

--- app/src/main/java/org/t_robop/triclo/triclo/ClRegistActivity.java
+++ app/src/main/java/org/t_robop/triclo/triclo/ClRegistActivity.java
@@ -1,16 +1,23 @@
 package org.t_robop.triclo.triclo;
 
 import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -40,19 +47,35 @@ public class ClRegistActivity extends AppCompatActivity {
         //Toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         //spinner
         Spinner jan_spinner = (Spinner) findViewById(R.id.jan_spi);
+
+
+        //動く気しない
+        /*cloName.setOnFocusChangeListener(new View.OnFocusChangeListener(){
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus){
+                    //受け取った時
+                    inputMethodManager.showSoftInput(v, InputMethodManager.SHOW_FORCED);
+                }else{
+                    //離れた時
+                    inputMethodManager.showSoftInput(v, InputMethodManager.HIDE_NOT_ALWAYS);
+                    mainLayout.requestFocus();
+                }
+            }
+        });*/
     }
 
     //scrollview外すと機能します
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
+    //@Override
+    public void onTouchEvent(View v) {
         // キーボードを隠す
-        inputMethodManager.hideSoftInputFromWindow(mainLayout.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        inputMethodManager.showSoftInput(v, InputMethodManager.HIDE_NOT_ALWAYS);
+        //inputMethodManager.hideSoftInputFromWindow(mainLayout.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
         // 背景にフォーカスを移す
         mainLayout.requestFocus();
-        return true;
+        //return true;
     }
 
     public void showDatePickerDialog(View v) {
@@ -64,14 +87,39 @@ public class ClRegistActivity extends AppCompatActivity {
         DatePickerDialog datePicker = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                String date = year+"/"+monthOfYear+"/"+dayOfMonth;
+                monthOfYear=monthOfYear+1;
+                String date = year + "/" + monthOfYear + "/" + dayOfMonth;
                 Toast.makeText(ClRegistActivity.this, date, Toast.LENGTH_SHORT).show();
                 dateText.setText(date);
             }
-        },nyear, nmonth, nday);
+        }, nyear, nmonth, nday);
         datePicker.show();
-        
     }
 
+    //色選択のダイアログフラグメント
+    public static class ColorSelectDialogFragment extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+            LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View content = inflater.inflate(R.layout.dialog_colorselect, null);
+
+            builder.setView(content);
+            builder.setMessage("色選択").setNegativeButton("決定", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    //決定ボタン押したときの処理
+
+                }
+            });
+            return builder.create();
+        }
+    }
+
+    //ダイアログ表示
+    public void showColorSelectDialog(View v){
+        DialogFragment newFragment = new ColorSelectDialogFragment();
+        newFragment.show(getFragmentManager(), "color");
+    }
 
 }

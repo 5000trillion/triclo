656cf6303ec45e7d7d66082b346a662e341fa434
Thiroaki <zeus1447@gmail.com>
2017-08-06 23:23:15 +0900

    ダイアログ修正 キーボード閉じ カラバリなし

--- app/src/main/java/org/t_robop/triclo/triclo/ClRegistActivity.java
+++ app/src/main/java/org/t_robop/triclo/triclo/ClRegistActivity.java
@@ -15,86 +15,91 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Calendar;
 
+import io.realm.Realm;
+
 
 /**
  * Created by tomi on 2017/06/20.
  */
 public class ClRegistActivity extends AppCompatActivity {
+
+    Realm realm;
+    TextView dateText;
+    TextView colText;
+    int nowyear;
+    int nowmonth;
+    int nowday;
+
     InputMethodManager inputMethodManager;
     private LinearLayout mainLayout;
-    TextView dateText;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clregist);
+
+        //キーボード閉じの初期化
         inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         mainLayout = (LinearLayout) findViewById(R.id.main_layout);
 
+        //DatePick用の初期化
+        final Calendar c = Calendar.getInstance();
+        nowyear = c.get(Calendar.YEAR);
+        nowmonth = c.get(Calendar.MONTH);
+        nowday = c.get(Calendar.DAY_OF_MONTH);
+
+        //TextView
         EditText cloName = (EditText) findViewById(R.id.editText1);
         dateText = (TextView) findViewById(R.id.dateText);
+        colText = (TextView) findViewById(R.id.coltext);
 
         //Toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        //spinner
-        Spinner jan_spinner = (Spinner) findViewById(R.id.jan_spi);
 
+        //Spinner
+        Spinner janSpinner = (Spinner) findViewById(R.id.jan_spi);
+        Spinner seasonSpinner = (Spinner) findViewById(R.id.season_spi) ;
 
-        //動く気しない
-        /*cloName.setOnFocusChangeListener(new View.OnFocusChangeListener(){
+        /* 何もないところをタップでキーボード閉じます。
+         */
+        mainLayout.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(hasFocus){
-                    //受け取った時
-                    inputMethodManager.showSoftInput(v, InputMethodManager.SHOW_FORCED);
-                }else{
-                    //離れた時
-                    inputMethodManager.showSoftInput(v, InputMethodManager.HIDE_NOT_ALWAYS);
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    inputMethodManager.hideSoftInputFromWindow(mainLayout.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
                     mainLayout.requestFocus();
                 }
+                return true;
             }
-        });*/
+        });
+
     }
 
-    //scrollview外すと機能します
-    //@Override
-    public void onTouchEvent(View v) {
-        // キーボードを隠す
-        inputMethodManager.showSoftInput(v, InputMethodManager.HIDE_NOT_ALWAYS);
-        //inputMethodManager.hideSoftInputFromWindow(mainLayout.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-        // 背景にフォーカスを移す
-        mainLayout.requestFocus();
-        //return true;
-    }
 
     public void showDatePickerDialog(View v) {
-        final Calendar c = Calendar.getInstance();
-        int nyear = c.get(Calendar.YEAR);
-        int nmonth = c.get(Calendar.MONTH);
-        int nday = c.get(Calendar.DAY_OF_MONTH);
-
         DatePickerDialog datePicker = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                monthOfYear=monthOfYear+1;
+                monthOfYear = monthOfYear + 1;
                 String date = year + "/" + monthOfYear + "/" + dayOfMonth;
+                nowyear = year;
+                nowmonth = monthOfYear - 1;
+                nowday = dayOfMonth;
                 Toast.makeText(ClRegistActivity.this, date, Toast.LENGTH_SHORT).show();
                 dateText.setText(date);
             }
-        }, nyear, nmonth, nday);
+        }, nowyear, nowmonth, nowday);
         datePicker.show();
     }
+
 
     //色選択のダイアログフラグメント
     public static class ColorSelectDialogFragment extends DialogFragment {
@@ -106,7 +111,7 @@ public class ClRegistActivity extends AppCompatActivity {
             View content = inflater.inflate(R.layout.dialog_colorselect, null);
 
             builder.setView(content);
-            builder.setMessage("色選択").setNegativeButton("決定", new DialogInterface.OnClickListener() {
+            builder.setMessage("色選択").setNegativeButton("キャンセル", new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int id) {
                     //決定ボタン押したときの処理
 
@@ -115,11 +120,18 @@ public class ClRegistActivity extends AppCompatActivity {
             return builder.create();
         }
     }
-
-    //ダイアログ表示
-    public void showColorSelectDialog(View v){
-        DialogFragment newFragment = new ColorSelectDialogFragment();
+    DialogFragment newFragment = new ColorSelectDialogFragment();
+    //表示
+    public void showColorSelectDialog(View v) {
         newFragment.show(getFragmentManager(), "color");
+    }
+
+
+    public void colorSelected(View v) {
+        String tagstr = String.valueOf(v.getTag());
+        colText.setText(tagstr);
+        newFragment.dismiss();
+        Toast.makeText(this, tagstr + "が選択されました", Toast.LENGTH_SHORT).show();
     }
 
 }

fd52a55453ba3a0820bb9f57082d221c9828e176
arika <arika.esah@gmail.com>
2017-08-07 10:00:17 +0900

    ImgCodeBottoms作りました

--- app/src/main/java/org/t_robop/triclo/triclo/ImgCodeBottomsActivity.java
+++ app/src/main/java/org/t_robop/triclo/triclo/ImgCodeBottomsActivity.java
@@ -1,44 +1,42 @@
 package org.t_robop.triclo.triclo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TabHost;
 
-public class ImgCodeBottomsActivity extends AppCompatActivity {
 
-    @Override
+
+
+
+
+public class ImgCodeBottomsActivity extends TabActivity {
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imgcodebottoms);
 
-        Button returnButton = (Button) findViewById(R.id.button);
-        returnButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
 
         //TabHostオブジェクト取得
         TabHost tabhost = (TabHost) findViewById(android.R.id.tabhost);
         tabhost.setup();
 
         TabHost.TabSpec tab1 = tabhost.newTabSpec("tab1");
-        tab1.setIndicator("タブ1");
+        tab1.setIndicator("全て");
         tab1.setContent(R.id.tab1);
         tabhost.addTab(tab1);
 
         TabHost.TabSpec tab2 = tabhost.newTabSpec("tab2");
-        tab2.setIndicator("タブ２");
+        tab2.setIndicator("ズボン");
         tab2.setContent(R.id.tab2);
         tabhost.addTab(tab2);
 
         TabHost.TabSpec tab3 = tabhost.newTabSpec("tab3");
-        tab3.setIndicator("タブ３");
+        tab3.setIndicator("スカート");
         tab3.setContent(R.id.tab3);
         tabhost.addTab(tab3);
 
@@ -53,4 +51,3 @@ public class ImgCodeBottomsActivity extends AppCompatActivity {
 
     }
 }
-

b762aa85a21264c4da76d9a6846b321018eaf931
arika <arika.esah@gmail.com>
2017-08-15 16:24:28 +0900

    Toolbar実装してます

--- app/src/main/java/org/t_robop/triclo/triclo/CodePickActivity.java
+++ app/src/main/java/org/t_robop/triclo/triclo/CodePickActivity.java
@@ -4,7 +4,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import static android.R.attr.id;
 
 /**
  * Created by arika on 2017/07/06.
@@ -19,9 +25,33 @@ public class CodePickActivity extends Activity {
 
         //Toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-
-
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_codepick,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        TextView varTextView = (TextView) findViewById(R.id.textView);
+        switch (item.getItemId()){
+            case R.id.item1:
+                varTextView.setText(R.string.menu_item1);
+                return true;
+            case R.id.item2:
+                varTextView.setText(R.string.menu_item2);
+                return true;
+            case R.id.item3:
+                varTextView.setText(R.string.menu_item3);
+                return true;
+            }
+        return super.onOptionsItemSelected(item);
+        }
+
 
 
     public void intentaccessory(View v){

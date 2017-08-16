73fb207c220a6c4ed58000447eda25e802bf7062
Ishihara1222 <7bjt1222@mail.u-tokai.ac.jp>
2017-08-16 15:45:24 +0900

    gridviewに何もないときトーストが出るようにしました。

--- app/src/main/java/org/t_robop/triclo/triclo/CameraActivity.java
+++ app/src/main/java/org/t_robop/triclo/triclo/CameraActivity.java
@@ -1,6 +1,9 @@
 package org.t_robop.triclo.triclo;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -9,13 +12,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 /**
  * Created by siilo on 2017/06/20.
  */
 
 public class CameraActivity extends AppCompatActivity {
-
+    private final static int RESULT_CAMERA = 1001;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,5 +29,19 @@ public class CameraActivity extends AppCompatActivity {
         //Toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        imageView = (ImageView)findViewById(R.id.image_view);
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, RESULT_CAMERA);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RESULT_CAMERA) {
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(bitmap);
+        }
     }
 }
+

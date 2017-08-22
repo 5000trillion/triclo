bb34959617d412bc31b5e778999374093082d319
arika <arika.esah@gmail.com>
2017-08-22 17:10:03 +0900

    toolbar

--- app/src/main/java/org/t_robop/triclo/triclo/CodePickActivity.java
+++ app/src/main/java/org/t_robop/triclo/triclo/CodePickActivity.java
@@ -3,8 +3,14 @@ package org.t_robop.triclo.triclo;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 
 /**
@@ -22,6 +28,38 @@ public class CodePickActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //Drawer
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.DrawerLayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar,
+                R.string.drawer_open,
+                R.string.drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        //NavigationView Listener
+        NavigationView navigationView = (NavigationView) findViewById(R.id.NavigationView);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_item1:
+                        Log.d("TAG", "Item 1");
+
+                        Intent intent = new Intent(CodePickActivity.this, Tab1_Activity.class);
+
+
+                        startActivity(intent);
+                }
+
+
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.DrawerLayout);
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+
+
+        });
     }
 
     public void intentaccessory(View v) {

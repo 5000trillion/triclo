135650544dd582594cda6cc843a9ecc3e441f1c4
arika <arika.esah@gmail.com>
2017-08-23 18:44:58 +0900

    CodeRegist作りました

--- app/src/main/java/org/t_robop/triclo/triclo/ImgAccessoryNeckActivity.java
+++ app/src/main/java/org/t_robop/triclo/triclo/ImgAccessoryNeckActivity.java
@@ -3,11 +3,11 @@ package org.t_robop.triclo.triclo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ImgAccessoryBraceletActivity extends AppCompatActivity {
+public class ImgAccessoryNeckActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_img_accessory_bracelet);
+        setContentView(R.layout.activity_img_accessory_neck);
     }
 }
